fd00cb50d035025e6c0a8d66b97db4f23bbc3bec
prajapatishubham336 <prajapatishubham336@gmail.com>
2026-08-01 00:06:37 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'Generative AI Project',
     version= '0.0.0',
     author= 'Shubham Prajapari',
-    author_email= 'prajapatishubhma336@gmail.com',
+    author_email= 'your gmail',
     packages= find_packages(),
     install_requires = []
 
